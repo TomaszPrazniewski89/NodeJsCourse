41275d01b9a4c4c74a4f0218a35147dd4337836f
Tomasz Prazniewski <tomaszprazniewski@MacBook-Pro-Tomasz.local>
2022-03-31 15:42:05 +0200

    Add function

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,5 +3,6 @@
 // // fs.writeFileSync('notes.txt','Test2 Created by Node.js')
 // fs.appendFileSync('notes.txt','append This file')
 
-const add = require('./utils.js')
-console.log(add(5,6))
+const notes = require('./notes')
+
+console.log(notes())
